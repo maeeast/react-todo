3da3cc50e3bd16d6f30a0a95cb53ba868af15b1f
Jenn Maher <maeeast@me.com>
2020-05-16 11:09:57 -0400

    Updates

---
+++ precache-manifest.651dc562a7f05c0f65fbd2561354445c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2342087d3a58611084fad3336e26a623",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "0c82325fb1bbdbb5c152",
+    "url": "/react-todo/static/css/main.4cf3bb71.chunk.css"
+  },
+  {
+    "revision": "26d77d1183cf74d6bb93",
+    "url": "/react-todo/static/js/2.081cbd43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.081cbd43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c82325fb1bbdbb5c152",
+    "url": "/react-todo/static/js/main.19d36cba.chunk.js"
+  },
+  {
+    "revision": "a7fb28e01dcc192be58f",
+    "url": "/react-todo/static/js/runtime-main.59b49ada.js"
+  }
+]);
