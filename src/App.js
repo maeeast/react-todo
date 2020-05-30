1ea128fef7cb0b9cd6960b99d5150a2d79a9119e
Jenn Maher <maeeast@me.com>
2020-05-30 13:52:54 -0400

    Adding delete command for firebase

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Todo from './Todo';
 import './App.css';
+import db from './firebase';
 
 function App() {
-  function deleteTodo(e) {
+  const handleDelete = (e) => {
     e.preventDefault();
-    let index = e.target.value;
-    todos.splice(index,1);
-    setTodos([...todos]);
+    // let index = e.target.value;
+    // todos.splice(index,1);
+    // setTodos([...todos]);
+    let index = parseInt(e.target.value);
+    let deleteId = '';
+
+    console.log(typeof index);
+
+    db.collection("todos").where("index", "==", index)
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          // console.log("DocID= " + doc.id);
+          deleteId = doc.id;
+          console.log(doc.id, " => ", doc.data());
+        });
+    })
+    .then(() =>{
+      db.collection('todos').doc(deleteId).delete().then(() =>{
+        console.log("Document successfully deleted!");
+    }).catch((error) => {
+        console.error("Error removing document: ", error);
+    });
+    })
   }
+
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
 
+useEffect(() => {
+  db.collection('todos').orderBy("index", "asc").onSnapshot(snapshot => {
+    setTodos(snapshot.docs.map(doc => doc.data()));
+  })
+}, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setTodos([...todos, input]);
+    // setTodos([...todos, input]);
+    db.collection('todos').add({
+      title: input,
+      index: Date.now(),
+    });
+
     setInput('');
   };
 
@@ -37,12 +71,12 @@ function App() {
           </form>
         </div> 
         
-        {todos.map((todo,index) => (
+        {todos.map((todo) => (
             <Todo 
-              key={index}
-              value={index}
-              title={todo}
-              deleteTodo={deleteTodo}
+              key={todo.index}
+              value={todo.index}
+              title={todo.title}
+              deleteTodo={handleDelete}
              />
         ))}
         
